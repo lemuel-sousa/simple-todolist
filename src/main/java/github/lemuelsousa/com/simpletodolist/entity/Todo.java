955bdd306352c0878d6f5b0ce651cfe0fac93537
lemuel de sousa <lemuel124.ldsp@gmail.com>
2023-07-27 06:16:00 -0300

    feat: create todo entity

---
+++ src/main/java/github/lemuelsousa/com/simpletodolist/entity/Todo.java
@@ -0,0 +1,35 @@
+package github.lemuelsousa.com.simpletodolist.entity;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+
+@Entity
+@Table( name = "todos")
+public class Todo {
+    
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private boolean finished;
+    private int priority;
+
+    public Todo(){}
+
+    public Todo(String name, String description, boolean finished, int priority) {
+        this.name = name;
+        this.description = description;
+        this.finished = finished;
+        this.priority = priority;
+    }
+ 
+    
+
+
+}
