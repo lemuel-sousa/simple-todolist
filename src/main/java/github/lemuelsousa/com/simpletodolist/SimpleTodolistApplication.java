dec82ff5e3f9f4dce95c174fad42555d2e1803d4
lemuel de sousa <lemuel124.ldsp@gmail.com>
2023-07-26 21:05:41 -0300

    start project

---
+++ src/main/java/github/lemuelsousa/com/simpletodolist/SimpleTodolistApplication.java
@@ -0,0 +1,13 @@
+package github.lemuelsousa.com.simpletodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTodolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/lemuelsousa/com/simpletodolist/SimpleTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package github.lemuelsousa.com.simpletodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
