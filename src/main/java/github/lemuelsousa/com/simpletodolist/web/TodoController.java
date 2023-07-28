9d62fc24c7d7b28a8deff20be1aa4b88a388e55a
lemuel de sousa <lemuel124.ldsp@gmail.com>
2023-07-27 21:43:27 -0300

    feat: refactor controller

--- src/main/java/github/lemuelsousa/com/simpletodolist/web/TodoController.java
+++ src/main/java/github/lemuelsousa/com/simpletodolist/web/TodoController.java
@@ -2,9 +2,15 @@ package github.lemuelsousa.com.simpletodolist.web;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import github.lemuelsousa.com.simpletodolist.entity.Todo;
 import github.lemuelsousa.com.simpletodolist.service.TodoService;
 
+@RequestMapping("/todos")
 public class TodoController {
     
     private TodoService todoService;
@@ -13,6 +19,8 @@ public class TodoController {
         this.todoService = todoService;
     }
 
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<Todo> getAll(){
         return todoService.list();
     }
