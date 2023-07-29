cf988f389dc8eb63b4f7e8006ad0b0455ec425ee
lemuel de sousa <lemuel124.ldsp@gmail.com>
2023-07-29 15:24:49 -0300

    feat: add service functions

--- src/main/java/github/lemuelsousa/com/simpletodolist/service/TodoService.java
+++ src/main/java/github/lemuelsousa/com/simpletodolist/service/TodoService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import github.lemuelsousa.com.simpletodolist.entity.Todo;
+import github.lemuelsousa.com.simpletodolist.exceptions.BadRequestException;
 import github.lemuelsousa.com.simpletodolist.repository.TodoRepository;
 
 @Service
@@ -17,10 +18,32 @@ public class TodoService {
     }
 
 
+    public List<Todo> create(Todo todo){
+        todoRepository.save(todo);        
+        return list();
+    }
+    
     public List<Todo> list(){
         return todoRepository.findAll();
     }
 
+    public List<Todo> update(Long id, Todo todo){
+        
+        todoRepository.findById(id).ifPresentOrElse(existingTodo -> {
+            todo.setId(id);
+            todoRepository.save(todo);
+        }, () -> {
+            throw new BadRequestException("Todo %d não existe!".formatted(id));
+        });
+        
+        todoRepository.save(todo);
+        return list();
+    }
 
+
+    public List<Todo> delete(Long id){
+        todoRepository.deleteById(id);
+        return list();
+    }
 
 }
