0a82cb50665fe719a0c5baf503d753ffd7828159
lemuel de sousa <lemuel124.ldsp@gmail.com>
2023-07-29 15:21:39 -0300

    feat: add getters and setters

--- src/main/java/github/lemuelsousa/com/simpletodolist/entity/Todo.java
+++ src/main/java/github/lemuelsousa/com/simpletodolist/entity/Todo.java
@@ -27,8 +27,50 @@ public class Todo {
         this.finished = finished;
         this.priority = priority;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isFinished() {
+        return finished;
+    }
+
+    public void setFinished(boolean finished) {
+        this.finished = finished;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
  
     
+    
+
 
 
 }
