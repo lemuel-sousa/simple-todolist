b7535b124d97bf4ba4687f7bf4e1a72bc3ac19f6
lemuel de sousa <lemuel124.ldsp@gmail.com>
2023-07-29 15:28:18 -0300

    feat: add seed

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+-- SEED
+INSERT INTO todos (name, description, finished, priority) VALUES ('Descansar', 'dormir 8 horas', false, 0);
+INSERT INTO todos (name, description, finished, priority) VALUES ('Descartar lixo', 'Recolher lixo do quintal', true, 1);
+INSERT INTO todos (name, description, finished, priority) VALUES ('Tomar remédios', 'Tomar vitaminas depois das refeições', false, 1);
+INSERT INTO todos (name, description, finished, priority) VALUES ('Verificar emails', '', false, 1);
