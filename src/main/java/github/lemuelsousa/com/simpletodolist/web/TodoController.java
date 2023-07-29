b38c1eab71976deb83d1c2f2d186053a32eb63c4
lemuel de sousa <lemuel124.ldsp@gmail.com>
2023-07-29 15:28:06 -0300

    feat: add API endpoints

--- src/main/java/github/lemuelsousa/com/simpletodolist/web/TodoController.java
+++ src/main/java/github/lemuelsousa/com/simpletodolist/web/TodoController.java
@@ -2,27 +2,51 @@ package github.lemuelsousa.com.simpletodolist.web;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import github.lemuelsousa.com.simpletodolist.entity.Todo;
 import github.lemuelsousa.com.simpletodolist.service.TodoService;
 
+@RestController
 @RequestMapping("/todos")
 public class TodoController {
-    
+
+    @Autowired
     private TodoService todoService;
 
-    public TodoController(TodoService todoService){
-        this.todoService = todoService;
-    }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public List<Todo> create(@RequestBody Todo todo){
+        return todoService.create(todo);
+    }
+    
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<Todo> getAll(){
         return todoService.list();
     }
+
+    @PutMapping("{id}")
+    public List<Todo> update(@PathVariable Long id, @RequestBody Todo todo){
+        return todoService.update(id, todo);
+    }
+
+    @DeleteMapping("{id}")
+    public List<Todo> delete(@PathVariable("id") Long id){
+        return todoService.delete(id);
+    }
+
+
 
 }
