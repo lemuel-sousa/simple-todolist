9592298a7573f7a6e16369a955b60687c438bc55
lemuel de sousa <lemuel124.ldsp@gmail.com>
2023-08-25 16:57:59 -0300

    feat: create successful todo update test

--- src/test/java/github/lemuelsousa/com/simpletodolist/SimpleTodolistApplicationTests.java
+++ src/test/java/github/lemuelsousa/com/simpletodolist/SimpleTodolistApplicationTests.java
@@ -5,17 +5,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import github.lemuelsousa.com.simpletodolist.DTO.TodoDTO;
-import github.lemuelsousa.com.simpletodolist.entity.Todo;
+import github.lemuelsousa.com.simpletodolist.repository.TodoRepository;
 
-@ActiveProfiles("test")
 @SpringBootTest( webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@Sql("/remove.sql")
 class SimpleTodolistApplicationTests {
 
 	@Autowired
 	private WebTestClient webTestClient;
+
+	@Autowired
+	private TodoRepository todoRepository;	
 
 	@Test
 	void testCreateTodoSuccess() {
@@ -40,16 +45,40 @@ class SimpleTodolistApplicationTests {
 	@Test
 	void testCreateTodoFailure() {
 		
+		var invalidTodo = new TodoDTO("", "", false, 1);
+
 		webTestClient
 			.post()
 			.uri("/todos")
-			.bodyValue(
-				new Todo("", "", false, 0))
+			.bodyValue(invalidTodo)
 			.exchange()
 			.expectStatus().isBadRequest();
 	}
 
+	@Test
+	@Sql("/import.sql")
+	void testUpdateTodoSuccess(){
+
+		var todo = todoRepository.findAll().get(0);
+		
+		var todoUp = new TodoDTO(todo.getId(), todo.getName() + " Up", todo.getDescription() + " Up", !todo.isFinished(), todo.getPriority() + 1); 
+
+		webTestClient
+			.put()
+			.uri("/todos/" + todoUp.getId())
+			.bodyValue(todoUp)
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody()
+			.jsonPath("$").isArray()
+			.jsonPath("$[0].length()").isEqualTo(5)
+			.jsonPath("$[0].name").isEqualTo(todoUp.getName())
+			.jsonPath("$[0].description").isEqualTo(todoUp.getDescription())
+			.jsonPath("$[0].finished").isEqualTo(todoUp.isFinished())
+			.jsonPath("$[0].priority").isEqualTo(todoUp.getPriority());
+	}
 
 
+	
 
 }
