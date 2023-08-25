403f1aef249702f76603f839ded1124746836920
lemuel de sousa <lemuel124.ldsp@gmail.com>
2023-08-25 18:10:39 -0300

    feat: create bad request exception handler

---
+++ src/main/java/github/lemuelsousa/com/simpletodolist/web/GeneralHandlerExcepion.java
@@ -0,0 +1,20 @@
+package github.lemuelsousa.com.simpletodolist.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import github.lemuelsousa.com.simpletodolist.exceptions.BadRequestException;
+
+@ControllerAdvice
+public class GeneralHandlerExcepion {
+    
+    @ExceptionHandler(BadRequestException.class)
+    private ResponseEntity<Object> handlerBadRequest(BadRequestException exception){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(exception.getMessage());
+    }
+
+}
