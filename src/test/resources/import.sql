050ef9fa0db6b1fc51fd6d3979ba7bd6d17680e4
lemuel de sousa <lemuel124.ldsp@gmail.com>
2023-09-01 15:12:32 -0300

    update

--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -1,5 +1,4 @@
--- SEED
-INSERT INTO todos (name, description, finished, priority) VALUES ('Descansar', 'dormir 8 horas', false, 0);
-INSERT INTO todos (name, description, finished, priority) VALUES ('Descartar lixo', 'Recolher lixo do quintal', true, 1);
-INSERT INTO todos (name, description, finished, priority) VALUES ('Tomar remédios', 'Tomar vitaminas depois das refeições', false, 1);
-INSERT INTO todos (name, description, finished, priority) VALUES ('Verificar emails', '', false, 1);
+INSERT INTO todos (id, name, description, finished, priority) VALUES (996, 'Descansar', 'dormir 8 horas', false, 0);
+INSERT INTO todos (id, name, description, finished, priority) VALUES (997, 'Descartar lixo', 'Recolher lixo do quintal', true, 1);
+INSERT INTO todos (id, name, description, finished, priority) VALUES (998, 'Tomar remédios', 'Tomar vitaminas depois das refeições', false, 1);
+INSERT INTO todos (id, name, description, finished, priority) VALUES (999, 'Verificar emails', 'limpar spam', false, 1);
