e7519e7cf8b82b385d5d8f5c483dabf5846983c4
lemuel de sousa <lemuel124.ldsp@gmail.com>
2023-09-05 15:31:51 -0300

    refact: handler exception

--- src/main/java/github/lemuelsousa/com/simpletodolist/web/GeneralHandlerExcepion.java
+++ src/main/java/github/lemuelsousa/com/simpletodolist/web/GeneralHandlerExcepion.java
@@ -1,19 +1,45 @@
 package github.lemuelsousa.com.simpletodolist.web;
 
+
+import java.util.List;
+
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import github.lemuelsousa.com.simpletodolist.exceptions.BadRequestException;
+import github.lemuelsousa.com.simpletodolist.exceptions.ApiErrorMessage;
+import github.lemuelsousa.com.simpletodolist.exceptions.TodoNotFoundException;
 
-@ControllerAdvice
-public class GeneralHandlerExcepion {
+@RestControllerAdvice
+public class GeneralHandlerExcepion extends ResponseEntityExceptionHandler{
     
-    @ExceptionHandler(BadRequestException.class)
-    private ResponseEntity<Object> handlerBadRequest(BadRequestException exception){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-            .body(exception.getMessage());
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode statusCode, WebRequest webRequest){
+    
+        List<String> errors = ex.getBindingResult()
+            .getFieldErrors()
+            .stream()
+            .map(FieldError::getDefaultMessage)
+            .toList();
+
+        var apiErrorMessage = new ApiErrorMessage(statusCode, errors);
+
+        return new ResponseEntity<>(apiErrorMessage, apiErrorMessage.getStatusCode());
     }
 
+    @ExceptionHandler(TodoNotFoundException.class)
+    public ResponseEntity<Object> handleTodoNotFoundException(TodoNotFoundException ex) {
+
+        ApiErrorMessage apiErrorMessage = new ApiErrorMessage(HttpStatus.NOT_FOUND, ex.getMessage());
+
+        return new ResponseEntity<Object>(apiErrorMessage, new HttpHeaders(), apiErrorMessage.getStatusCode());
+    }
 }
