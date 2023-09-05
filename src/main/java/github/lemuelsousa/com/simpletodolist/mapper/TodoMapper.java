a8ec96a47e7e832ac6333d40a92b9b894cefd932
lemuel de sousa <lemuel124.ldsp@gmail.com>
2023-09-05 15:34:57 -0300

    update

---
+++ src/main/java/github/lemuelsousa/com/simpletodolist/mapper/TodoMapper.java
@@ -0,0 +1,36 @@
+package github.lemuelsousa.com.simpletodolist.mapper;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import github.lemuelsousa.com.simpletodolist.dto.RequestTodoDto;
+import github.lemuelsousa.com.simpletodolist.dto.ResponseTodoDto;
+import github.lemuelsousa.com.simpletodolist.entity.Todo;
+
+@Component
+public class TodoMapper {
+
+    public Todo toEntity(RequestTodoDto request){     
+            return new Todo(
+            request.name(),
+            request.description(),
+            request.finished(),
+            request.priority());
+    }
+    
+    public ResponseTodoDto toResponse(Todo todo){
+        return new ResponseTodoDto(
+            todo.getId(),
+            todo.getName(),
+            todo.getDescription(),
+            todo.isFinished(),
+            todo.getPriority()
+        );
+    }
+
+    public List<ResponseTodoDto> toListOfResponseTodo(List<Todo> todos){
+        return todos.stream().map(this::toResponse).toList();
+    }
+
+}
